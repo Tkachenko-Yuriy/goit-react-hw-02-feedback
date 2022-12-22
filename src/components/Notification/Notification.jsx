87f21cfe64f4a4862bce90c9fd5f4779dbd93783
Tkachenko_Yuriy <uvt86@ukr.net>
2022-12-22 14:05:25 +0200

    summary

--- src/components/Notification/Notification.jsx
+++ src/components/Notification/Notification.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { Message} from '../Notification/Notification.styled';
 
 
 export default class Notification extends Component {
@@ -8,6 +9,6 @@ export default class Notification extends Component {
   };
 
   render() {
-    return <p >{this.props.message}</p>;
+    return <Message>{this.props.message}</Message>;
   }
 }
